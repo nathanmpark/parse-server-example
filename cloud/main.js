8b21a1c3d5941ab04fee217198067e8f740c05ad
Nathan Park <nathan@objectlabs.com>
2016-03-30 17:20:04 -0700

    Add changes

--- cloud/main.js
+++ cloud/main.js
@@ -8,26 +8,26 @@ Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world! " + (request.params.a + request.params.b));
 });
 
-Parse.Cloud.beforeSave('TestObject', function(request, response) {
-  console.log('Ran beforeSave on objectId: ' + request.object.id);
-  // if update the request object, we need to send it back with the response
-  response.success(request.object);
-});
-
-Parse.Cloud.afterSave('TestObject', function(request, response) {
-  console.log('Ran afterSave on objectId: ' + request.object.id);
-});
-
-Parse.Cloud.beforeDelete('TestObject', function(request, response) {
-  console.log('Ran beforeDelete on objectId: ' + request.object.id);
-  response.success();
-});
-
-Parse.Cloud.afterDelete('TestObject', function(request, response) {
-  console.log('Ran afterDelete on objectId: ' + request.object.id);
-});
-
 Parse.Cloud.define("find", function(request, response) {
   console.log('Ran find function.');
   response.success("Find something...");
 });
+
+// Parse.Cloud.beforeSave('TestObject', function(request, response) {
+//   console.log('Ran beforeSave on objectId: ' + request.object.id);
+//   // if update the request object, we need to send it back with the response
+//   response.success(request.object);
+// });
+
+// Parse.Cloud.afterSave('TestObject', function(request, response) {
+//   console.log('Ran afterSave on objectId: ' + request.object.id);
+// });
+
+// Parse.Cloud.beforeDelete('TestObject', function(request, response) {
+//   console.log('Ran beforeDelete on objectId: ' + request.object.id);
+//   response.success();
+// });
+
+// Parse.Cloud.afterDelete('TestObject', function(request, response) {
+//   console.log('Ran afterDelete on objectId: ' + request.object.id);
+// });
