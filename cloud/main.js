e734a57c1c69cbcd77bc706a7a63fe31f9c82e71
Nathan Park <nathan@objectlabs.com>
2016-04-04 18:15:21 -0700

    update cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -20,12 +20,7 @@ Parse.Cloud.define("sendEmailToUser", function(request, response) {
 
 Parse.Cloud.define("hello", function(request, response) {
   console.log('Ran cloud function.');
-  // As with Parse-hosted Cloud Code, the user is available at: request.user
-  // You can get the users session token with: request.user.getSessionToken()
-  // Use the session token to run other Parse Query methods as that user, because
-  //   the concept of a 'current' user does not fit in a Node environment.
-  //   i.e.  query.find({ sessionToken: request.user.getSessionToken() })...
-  response.success("Hello world! " + (request.params.a + request.params.b));
+  response.success("Hello world!");
 });
 
 Parse.Cloud.define("find", function(request, response) {
