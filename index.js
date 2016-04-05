ee8dbea2eda03a1c22ac9cdc8902fd8c6839a25b
Nathan Park <nathan@objectlabs.com>
2016-04-04 18:25:08 -0700

    logs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Example express application adding the parse-server module to expose Parse
 // compatible API routes.
+console.log(__dirname);
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
